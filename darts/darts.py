3723fe3e11b89b6c55ccb81b6ebf61cacbe8243d
SreyasMaha <sreyas.mahadevan@gmail.com>
2023-01-20 16:43:49 +0000

    complete_darts.py

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,16 @@
+import math
+
 def score(x, y):
-    pass
+    p = math.sqrt(x**2 + y**2)
+    out = 10
+    if p > 1:
+        out = out - 5
+    
+    if p > 5:
+        out = out - 4
+    
+    if p > 10:
+        out = out - 1
+
+    return out 
+    
